347ac3a6382e7839e0cc76c3f9551b51195924fd
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-10-25 12:46:11 +0300

    CatchUp

--- Assets/Scripts/CatchUp/Companion.cs
+++ Assets/Scripts/CatchUp/Companion.cs
@@ -6,11 +6,19 @@ public class Companion : Follower
 {
     [SerializeField] private Transform _target;
     [SerializeField] private float _lengthRay;
+    [SerializeField] private Vector3 _offset;
+
+    private void Start()
+    {
+        _offset = transform.position - _leader.position;
+    }
 
     protected override void Move()
     {
+        transform.position = _leader.position + _offset;
         transform.RotateAround(_leader.position, Vector3.up, _speed);
         transform.LookAt(_target.position);
+        _offset = transform.position - _leader.position;
         Debug.DrawRay(transform.position, transform.forward * _lengthRay, Color.red);
     }
 }
