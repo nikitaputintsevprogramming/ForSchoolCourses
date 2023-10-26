6e75b115c0c91a09758b7653ecd2d1d9b4d6f88b
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-10-26 23:35:33 +0300

    Update Movement.cs
    доделано движение

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,6 +4,8 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _rotationSpeed = 180f;
+    [SerializeField] private float _jumpForce= 10f;
+    private bool isJumped;
 
     private Rigidbody rb;
 
@@ -11,25 +13,33 @@ public class Movement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
+        isJumped = false;
+    }
+
+    private void Update()
+    {
+        isJumped = Input.GetKeyDown(KeyCode.Space);
     }
 
     private void FixedUpdate()
     {
+        if (isJumped)
+        {
+            rb.AddForce(transform.up * _jumpForce * Time.fixedDeltaTime, ForceMode.Impulse);
+        }
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
         if (moveDirection != Vector3.zero)
-        {
-            Vector3 newPosition = transform.position + moveDirection * _speed * Time.deltaTime;
+        { 
+            //для физического движения
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
-            transform.position = newPosition;
-
-            //Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-            //rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime));
-            //rb.MovePosition(transform.position + moveDirection * _speed * Time.fixedDeltaTime);
+            rb.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime));
+            if(targetRotation == transform.rotation)
+                rb.MovePosition(transform.position + moveDirection * _speed * Time.fixedDeltaTime);
         }
     }
 }
