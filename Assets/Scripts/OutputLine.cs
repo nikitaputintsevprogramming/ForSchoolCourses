fc5f1213c582d03d0eca8edbf3b98f9ad10a056d
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-10-31 11:06:47 +0300

    записал тему

---
+++ Assets/Scripts/OutputLine.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutputLine : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log("Hello, world!");
+    }
+}
