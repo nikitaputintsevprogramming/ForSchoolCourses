8407a8098cbc7554f48b6d81b77f157632ae8866
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-10-31 15:11:53 +0300

    записал тему

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _rotationCenter;
+    [SerializeField] private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = transform.position - _rotationCenter.position;
+    }
+
+    void Update()
+    {
+        transform.position = _rotationCenter.position + _offset;
+        transform.RotateAround(_rotationCenter.position, Vector3.up, _speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * _speed * Time.deltaTime);
+        _offset = transform.position - _rotationCenter.position;
+    }
+}
