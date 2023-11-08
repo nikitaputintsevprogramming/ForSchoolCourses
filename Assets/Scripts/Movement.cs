919c84ea5f0bfb32c37f821006e450db6ad135b5
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-08 23:04:00 +0300

    доделал

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,23 +6,43 @@ public class Movement : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _jumpForce;
+
+    private bool isJumped;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.freezeRotation = true;
+        isJumped = false;
+    }
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal"); 
+        isJumped = Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private void FixedUpdate()
+    {
+        if(isJumped)
+        {
+            _rigidbody.AddForce(transform.up * _jumpForce * Time.deltaTime, ForceMode.Impulse);
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
-        if(moveDirection != Vector3.zero)
+        if (moveDirection != Vector3.zero)
         {
-            Vector3 newPosition = transform.position + moveDirection * _speed * Time.deltaTime;
             Quaternion targetRotation = Quaternion.LookRotation(moveDirection, Vector3.up);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
-            
-            if(transform.rotation == targetRotation)
+            _rigidbody.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.fixedDeltaTime));
+
+            if (targetRotation == transform.rotation )
             {
-                transform.position = newPosition;
+                _rigidbody.MovePosition(transform.position + moveDirection * _speed * Time.fixedDeltaTime);
             }
         }
     }
