d47ec0b3f68fe260cd7073fbf06d787c77c23d9a
nikitaputintsevprogramming <100438132+nikitaputintsevprogramming@users.noreply.github.com>
2023-11-27 22:03:22 +0300

    записал 1 лекцию

---
+++ Assets/Scripts/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleLogger : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("This is Start!");
+    }
+
+    private void Update()
+    {
+        Debug.Log("This is Update!");
+    }
+}
